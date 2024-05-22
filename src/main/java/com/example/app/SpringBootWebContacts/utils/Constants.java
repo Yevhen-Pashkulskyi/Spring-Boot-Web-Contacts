a4de72fa8c1945f8c96fd25c1b83bdbe74a19b40
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-22 18:30:52 +0300

    created constants

---
+++ src/main/java/com/example/app/SpringBootWebContacts/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.app.SpringBootWebContacts.utils;
+
+public class Constants {
+    public static final String URL_IMAGES = "/images/";
+    public static final String URL_FILES = "/static/files/";
+    public static final String FILE_TEAM_MAIN = "team_main.json";
+}
